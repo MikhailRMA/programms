c655eae2a0aee3eec3dc5c41ca5b8f181765c3da
Михаил <majkl.a@yandex.ru>
2025-10-30 15:57:11 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -435,7 +435,25 @@ def get_csv_download_link(sku_list, filename):
 
 def main():
 
-  
+  # Яндекс.Метрика через st.markdown
+    metrika_code = """
+    <script>
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+        ym(104969939, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/104969939" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    """
+    
+    st.markdown(metrika_code, unsafe_allow_html=True)
 
 
     # Кастомный заголовок
